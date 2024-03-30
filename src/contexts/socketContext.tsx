d7896f8182d2d26b04936de84164d170263f8542
Jabirrr <jabirmhd6786@gmail.com>
2024-03-30 11:41:10 +0530

    socket-test2

--- src/contexts/socketContext.tsx
+++ src/contexts/socketContext.tsx
@@ -20,7 +20,7 @@ export const SocketProvider: React.FC<SocketContextProps> = ({ children }) => {
   
   useEffect(() => {
     // const newSocket = io("http://localhost:3005");
-    const newSocket = io("https://web.careerflow.shop/");
+    const newSocket = io("https://careerflow.shop/");
     
     console.log("🚀 ~ file: socketContext.tsx:19 ~ socket:", socket)
     setSocket(newSocket);
