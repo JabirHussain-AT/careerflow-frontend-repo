7094bb4428e623bcd9b25826496b250e3825a2a5
Jabirrr <jabirmhd6786@gmail.com>
2024-04-01 17:14:31 +0530

    some design updates 0.0.9

--- src/components/user/Profile/MiniDash.tsx
+++ src/components/user/Profile/MiniDash.tsx
@@ -11,19 +11,19 @@ const MiniDash: React.FC = () => {
     <div className="bg-gradient-to-b from-gray-200 to-orange-100 text-white p-5 w-full rounded-lg gap-2  md:gap-0  flex items-center justify-between">
       {" "}
       {/* No Applications */}
-      <div className="bg-blue-600 h-24 w-32 p-4 rounded shadow-md text-start">
+      <div className="bg-blue-600 h-24 md:w-32 w-36  p-4 rounded shadow-md text-start">
         <FaTasks className="text-2xl" />
         <span className="font-bold text-xs ">Applications : 0 </span>
       </div>
       {/* No Interviews Scheduled */}
-      <div className="bg-green-600 h-24 w-32 p-4 rounded shadow-md text-start">
+      <div className="bg-green-600 h-24 md:w-32 w-36 p-4 rounded shadow-md text-start">
         <FaCalendarAlt className="text-2xl" />
         <span className="text-xs font-bold  mb-2">
           Interviews Scheduled: {noInterviewsScheduled}
         </span>
       </div>
       {/* No Saved Jobs */}
-      <div className="bg-yellow-600 h-24 w-24 p-4 rounded shadow-md text-start">
+      <div className="bg-yellow-600 md:h-24 md:w-24 w:32 p-4 rounded shadow-md text-start">
         <CiSaveDown2 className="text-2xl font-bold" />
         <span className="text-xs font-bold  mb-2">Saved Jobs: 21</span>
       </div>
