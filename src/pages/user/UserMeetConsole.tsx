54f53af8bc85160273a4c86a0e7ad7ae8e01155c
Jabirrr <jabirmhd6786@gmail.com>
2024-04-06 11:26:58 +0530

    fix : interview meet 0.0.9

--- src/pages/user/UserMeetConsole.tsx
+++ src/pages/user/UserMeetConsole.tsx
@@ -1,39 +1,58 @@
-import React from 'react' 
-import { useNavigate, useParams } from 'react-router-dom'
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt'
-import NavBar from '@/components/user/Home/NavBar'
-import { useSelector } from 'react-redux'
-import { IUserSelector } from '@/interface/IUserSlice'
+import React, { useEffect, useRef } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import NavBar from '@/components/user/Home/NavBar';
+import { useSelector } from 'react-redux';
+import { IUserSelector } from '@/interface/IUserSlice';
 
-const UserMeetConsole : React.FC = () => {
-    
-    const { user } = useSelector((state: IUserSelector) => state.user);
-    const { roomId } = useParams()
-    const navigate = useNavigate()
-    const meeting = async ( element : any ) => {
-        const appID = 1916087609 ;
-        const serverSecret = "9452af6a726e6150cd728db8129dc18d" ;
-        const kitToken  = ZegoUIKitPrebuilt.generateKitTokenForTest(appID , serverSecret , roomId! , Date.now().toString(),`${user?.userName!}`)
-        const zc = ZegoUIKitPrebuilt.create( kitToken )
-        zc.joinRoom({   
-            container : element ,
-            onReturnToHomeScreenClicked :  () => {
-                navigate('/profile/my-interviews')
-                },
-            showLeaveRoomConfirmDialog : true ,
-            scenario : {
-                mode : ZegoUIKitPrebuilt.GroupCall ,
-          }}
-        )
-    }
+const UserMeetConsole: React.FC = () => {
+  const { user } = useSelector((state: IUserSelector) => state.user);
+  const { roomId } = useParams();
+  const navigate = useNavigate();
+  const meetingRef = useRef<HTMLDivElement>(null);
 
-        return <>
-        < NavBar />
-        <div className='bg-green-100 flex justify-center h-screen w-screen rounded-lg p-5 '>
-            <div className='w-10/12 h-5/6' ref={meeting} />
-        </div>
-        </>
+  useEffect(() => {
+    const meeting = async (element: HTMLDivElement | null) => {
+      if (!element) return;
 
-}
+      const appID = 1916087609;
+      const serverSecret = "9452af6a726e6150cd728db8129dc18d";
+      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+        appID,
+        serverSecret,
+        roomId!,
+        Date.now().toString(),
+        `${user?.userName!}`
+      );
+      const zc = ZegoUIKitPrebuilt.create(kitToken);
+      zc.joinRoom({
+        container: element,
+        onReturnToHomeScreenClicked: () => {
+          navigate('/profile/my-interviews');
+        },
+        showLeaveRoomConfirmDialog: true,
+        scenario: {
+          mode: ZegoUIKitPrebuilt.GroupCall,
+        },
+      });
 
-export default UserMeetConsole ;
+      // Return the destroy function to be executed when the component unmounts
+      return () => {
+        zc.destroy();
+      };
+    };
+
+    meeting(meetingRef.current);
+  }, [roomId, user, navigate]);
+
+  return (
+    <>
+      <NavBar />
+      <div className='bg-green-100 flex justify-center h-screen w-screen rounded-lg p-5 '>
+        <div className='w-10/12 h-5/6' ref={meetingRef} />
+      </div>
+    </>
+  );
+};
+
+export default UserMeetConsole;
