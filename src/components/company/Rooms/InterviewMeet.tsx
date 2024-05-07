2e0547375f03b7b6a70905c9a147d011b2830325
Jabir Hussain A T <138150152+JabirHussain-AT@users.noreply.github.com>
2024-05-07 11:04:47 -0700

    Update InterviewMeet.tsx

--- src/components/company/Rooms/InterviewMeet.tsx
+++ src/components/company/Rooms/InterviewMeet.tsx
@@ -14,8 +14,10 @@ const InterviewMeet: React.FC = () => {
     const meeting = async (element: HTMLDivElement | null) => {
       if (!element) return;
 
-      const appID = 1916087609;
-      const serverSecret = "9452af6a726e6150cd728db8129dc18d";
+      // const appID = 1916087609;
+      // const serverSecret = "9452af6a726e6150cd728db8129dc18d";
+      const appID = 46877336;
+      const serverSecret = "4ac3ffa5d4e7f2f54e4bb82b0c876db8";
       const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
         appID,
         serverSecret,
